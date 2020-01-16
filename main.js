238d66a73d605e32f8a23e942ad4ae76d9a20bd7
Ildar <ildarooo798@yandex.ru>
2020-01-17 00:39:24 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,58 @@
+'use strict'
+var number = 3;
+var string = "Inform!";
+var string = "Informs";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+/* console.log(4/0);
+console.log('string'*9);
+
+let sum;
+console.log(sum);
+
+let persona = {
+    fio: "Ivan",
+    age: 20,
+    isMarried: false
+};
+
+console.log(persona["fio"]);
+
+let foto = ['plum.png', 'orange.png', 'apple.bmp'];
+
+console.log(foto[1]);
+
+alert('Джава');
+
+confirm('Ты дома?');
+
+// let question = confirm ('Download in the games?');
+
+// console.log(question);
+
+let age = +prompt("Сколько вам лет?", "");
+
+console.log(typeof(null));
+
+ console.log("foto" + " - object");
+ console.log(4+ " - object"); 
+ */
+
+  let incr = 10,
+      decr = 10;
+
+
+  console.log(incr++);
+  console.log(decr--);
+  console.log(10%3)
+ console.log("2" === "2");
+
+ let isCheked = false;
+    isClose = false;
+
+    console.log(isCheked || !isClose);
+    console.log(3 ** 2);
